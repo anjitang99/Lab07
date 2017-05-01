c61be8e17b67ec3aa6eb17fdd8c4ec2a76828acd
ajou <ajou@ajou-PC>
2017-05-01 11:37:11 +0900

    i did datastorage and smartTV

---
+++ Lab07/src/SmartTV.java
@@ -0,0 +1,27 @@
+
+public class SmartTV extends Product{
+
+	private int channel;
+	public int getChannel() {
+		return channel;
+	}
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
+	public boolean isConnected()
+	{
+		return true;
+	}
+	
+	public double maxSpeed()
+	{
+		return 2;
+	}
+	@Override
+	public String getName()
+	{
+		return "samsung";
+	}
+	
+}
