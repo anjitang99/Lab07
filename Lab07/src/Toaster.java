de5dc1fd07ba0dbe32d6cfcf31559431ac3d7dc6
anjihoon <anjitang@naver.com>
2017-05-01 11:31:58 +0900

    Cooker and Toaster have been changed

---
+++ Lab07/src/Toaster.java
@@ -0,0 +1,17 @@
+
+public class Toaster extends Product implements Cooker {
+
+	@Override
+	public String prepareFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
